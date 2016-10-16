92757800574f6425b3f8d983d589d578ddd045f3
Timwi <github@timwi.de>
2016-10-16 15:15:08 +0200

    Minor changes

--- Assets/Assets/FollowTheLeaderModule.cs
+++ Assets/Assets/FollowTheLeaderModule.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using UnityEngine;
 using FollowTheLeaderNS;
-
+using UnityEngine;
 using Rnd = UnityEngine.Random;
 
 /// <summary>
--- Assets/Assets/MeshGenerator.cs
+++ Assets/Assets/MeshGenerator.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Linq;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
-using FollowTheLeaderNS;
 
 namespace FollowTheLeaderNS
 {
